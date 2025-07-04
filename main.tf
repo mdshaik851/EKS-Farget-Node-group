612317e3f076f60af71373020f541edfae2c2fd8
mdshaik851 <md939255shaik@gmail.com>
2025-07-04 16:09:38 +0530

    changes for VPC

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "./modules/vpc"
+  region = var.region
+  vpc_cidr = var.vpc_cidr
+  public_subnet = var.public_subnet
+  availability_zone = var.availability_zone
+  private_subnet = var.private_subnet
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "region" {
+    description = "Region"
+    type = string
+    default = "us-west-1"
+
+}
+
+variable "vpc_cidr" {
+    description = "VPC cidr range"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet" {
+    description = "public subnets descriptions"
+    type = list(string)
+    default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet" {
+    description = "private subnets descriptions"
+    type = list(string)
+    default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zone" {
+    description = "Availability Zones"
+    type = list(string)
+    default     = ["us-west-1a", "us-west-1b"]
+}
