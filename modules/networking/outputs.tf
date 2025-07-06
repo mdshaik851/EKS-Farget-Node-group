0f97c7922aac7644bb283a4de076fda8d134fb60
mdshaik851 <md939255shaik@gmail.com>
2025-07-06 12:47:18 +0530

    modified changes

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,8 +1,8 @@
 output "eks_security_group_id" {
-  value       = aws_security_group.eks.id
+  value       = aws_security_group.eks_clustersecurity_group.id
 }
 
 output "rds_security_group_aurora_id" {
-  value       = aws_security_group.rds_sg.id 
+  value       = aws_security_group.rds_security_group.id 
 }
 
