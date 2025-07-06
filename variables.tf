8d23ac64c5a86327e6d610ddf075704fcd8d3cef
mdshaik851 <md939255shaik@gmail.com>
2025-07-06 13:01:43 +0530

    added changes for RDS

--- variables.tf
+++ variables.tf
@@ -23,8 +23,27 @@ variable "private_subnet" {
     default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
+
 variable "availability_zone" {
     description = "Availability Zones"
     type = list(string)
     default     = ["ap-northeast-3a", "ap-northeast-3b"]
+}
+
+variable "name" {
+  description = "Name to be used on VPC created"
+  type        = string
+  default     = "demo6" 
+}
+
+variable "db_username" {
+  description = "Name of the db_username"
+  type        = string
+  default     = "admin"
+}
+
+variable "database_name" {
+  description = "Name of the db_name"
+  type        = string
+  default     = "test"
 }
