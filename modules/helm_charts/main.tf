8948f0ad6dd31baa63b7f1a414d341a08875229a
mdshaik851 <md939255shaik@gmail.com>
2025-07-06 16:15:01 +0530

    modified changes

--- modules/helm_charts/main.tf
+++ modules/helm_charts/main.tf
@@ -12,23 +12,11 @@ provider "kubernetes" {
 }
 
 # HELM Provider
-#provider "helm" {
-#  kubernetes {
-#    host                   = var.cluster_endpoint
-#    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-#    token                  = data.aws_eks_cluster_auth.cluster.token
-#  }
-#}
-
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_id]
-    }
+   cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
