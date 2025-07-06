7a2e2efdbf83ec8b90623c916586e2a242b8e00a
mdshaik851 <md939255shaik@gmail.com>
2025-07-06 12:43:00 +0530

    modifed changes

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket       = "uc-eks-farget-node-group-hybrid"
     key          = "uc-eks-farget-node-group-hybrid"
-    region       = "us-west-1"
+    region       = "ap-northeast-3"
     encrypt      = true
     use_lockfile = true
   }
