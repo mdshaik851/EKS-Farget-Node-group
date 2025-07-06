f3ca303f5b372b520e62c15918c1474f79c72b15
mdshaik851 <md939255shaik@gmail.com>
2025-07-06 14:52:34 +0530

    Added multiple changes

--- main.tf
+++ main.tf
@@ -23,12 +23,50 @@ module "rds" {
 module "iam" {
   source = "./modules/iam"
   name = var.name
-  aws_iam_openid_connect_provider_arn = module.eks.aws_iam_openid_connect_provider_arn
-  aws_iam_openid_connect_provider_extract_from_arn = module.eks.aws_iam_openid_connect_provider_extract_from_arn
+  aws_iam_openid_connect_provider_arn = module.eks_cluster.aws_iam_openid_connect_provider_arn
+  aws_iam_openid_connect_provider_extract_from_arn = module.eks_cluster.aws_iam_openid_connect_provider_extract_from_arn
 
   depends_on = [
     module.vpc
   ]
 
 }
+
+module "helm" {
+  source = "./modules/helm"
+  cluster_id = module.eks.cluster_id
+  cluster_endpoint = module.eks.cluster_endpoint
+  cluster_certificate_authority_data = module.eks.cluster_certificate_authority_data
+  lbc_iam_depends_on = module.iam.lbc_iam_depends_on
+  lbc_iam_role_arn   = module.iam.lbc_iam_role_arn
+  vpc_id             = module.vpc.vpc_id
+  aws_region         = var.region
+}
+
+
+
+module "eks" {
+  source = "./modules/eks_cluster"
+  name                = var.name
+  public_subnets      = module.vpc.public_subnets
+  private_subnets     = module.vpc.private_subnets
+  cluster_role_arn    = module.iam.eks_cluster_role_arn
+  node_role_arn       = module.iam.eks_node_role_arn
+  fargate_profile_role_arn = module.iam.fargate_profile_role_arn
+  eks_oidc_root_ca_thumbprint = var.eks_oidc_root_ca_thumbprint
+  cluster_role_dependency = module.iam.eks_role_depends_on
+  namespace_depends_on   = module.helm_charts.namespace_depends_on
+  namespace           = module.helm_charts.namespace
+  security_group_ids  = [module.vpc.eks_security_group_id]
+
+  depends_on = [
+    module.vpc
+  ]
+}
+
+
+module "ecr" {
+  source = "./modules/ecr"
+}
+
 
--- variables.tf
+++ variables.tf
@@ -47,3 +47,9 @@ variable "database_name" {
   type        = string
   default     = "test"
 }
+
+variable "eks_oidc_root_ca_thumbprint" {
+  type        = string
+  description = "Thumbprint of Root CA for EKS OIDC, Valid until 2037"
+  default     = "9e99a48a9960b14926bb7f3b02e22da2b0ab7280"
+}
